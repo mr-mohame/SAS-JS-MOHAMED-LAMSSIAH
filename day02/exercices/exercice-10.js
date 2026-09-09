0d1ae6986270526f2b0446f0a7efcabb97769748
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-09 12:21:35 +0100

    day 02 :termine les exercices enter 1 et 11 et les ghides 1, 2, 3

---
+++ day02/exercices/exercice-10.js
@@ -0,0 +1,25 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 02 · EXERCICE 10 · NIVEAU 2 : CONSOLIDATION (INTERMÉDIAIRES)
+ * LA FACTORIELLE
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Calculez la factorielle de 6 (écrit 6! = 6 * 5 * 4 * 3 * 2 * 1) avec une boucle.
+ *
+ * RÉSULTAT ATTENDU
+ * 720
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-10
+ * ▶️ Commande : node day02/exercices/exercice-10.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let fact = 6
+let resultat = 1
+for(let i = 1; i<= fact; i++){
+    resultat = resultat * i
+}
+console.log(`la factoriel de ${fact} est : ${resultat}`)
