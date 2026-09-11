043eb2099ee8359cf5369d47752e74bdef160a89
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-11 15:38:40 +0100

    day 04: termine le ex 13

--- day04/exercices/exercice-13.js
+++ day04/exercices/exercice-13.js
@@ -14,4 +14,18 @@
 
 // 1. Identifie les données nécessaires.
 // 2. Écris ta solution sous cette ligne.
-// TODO: écris ta solution ici.
+function genererMotDePasse(longueur){
+    let motDePasse = "";
+    for(let i=0; i<=longueur; i++){
+        if(Math.random()>0.5){
+            motDePasse += "A";
+        }else{
+            let chiffre = Math.floor(Math.random() * 10);
+            motDePasse += chiffre
+        }
+    }
+    return motDePasse;
+}
+
+console.log(genererMotDePasse(10))
+
