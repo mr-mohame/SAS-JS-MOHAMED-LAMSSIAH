72c3c7824fe642e74e7558dfc4064e83d442ba62
mr-mohame <mohamed.lamssiah9@gmail.com>
2026-09-15 16:48:49 +0100

    day07 : termine les guides et les exercices entre 1 et 6

---
+++ day07/exercices/exercice-02.js
@@ -0,0 +1,26 @@
+/**
+ * ─────────────────────────────────────────────────────────────
+ * JOUR 07 · EXERCICE 02 · NIVEAU 1 : FONDATIONS (DÉBUTANTS)
+ * MISE À JOUR
+ * ─────────────────────────────────────────────────────────────
+ *
+ * 🎯 MISSION
+ * Changez l'âge de l'utilisateur précédent pour l'augmenter de 1, et ajoutez-lui une propriété email.
+ *
+ * 📖 Consigne détaillée : ../03-exercices.md#exercice-02
+ * ▶️ Commande : node day07/exercices/exercice-02.js
+ */
+'use strict';
+
+// 1. Identifie les données nécessaires.
+// 2. Écris ta solution sous cette ligne.
+let utilisateur ={
+    nom: "mohamed",
+    age: 29,
+    estAdmin: true
+
+};
+
+utilisateur.age = 29;
+utilisateur.email = "mohamed@gmail.com"
+console.log(utilisateur)
